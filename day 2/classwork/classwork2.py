9150199b37a3d5e69d52ca317e59624848e6564e
narimanidzegoga <narimanidzegoga480@gmail.com>
2026-02-06 22:23:56 +0400

    third commit'

---
+++ day 2/classwork/classwork2.py
@@ -0,0 +1,5 @@
+word1 = "i"
+word2 = "study"
+word3 = "at"
+word4 = "GOA"
+print (word1+" "+ word2 +" "+ word3 + " " + word4)
---
+++ day 2/homework/homework2.py
@@ -0,0 +1,26 @@
+birth_year = 2010
+current_year = 2026
+age = current_year - birth_year
+print(age)
+
+
+number1 = 4
+number2 = 5
+number3 = 7
+number4 = 9
+number5 = 12
+print ((number1+number2+number3+number4+number5)/5)
+
+# დეკლარაცია - ნიშნავს კოდში თქმას რომ რაღაც არსებობს
+
+b = 8
+
+# ინიციალიზება - ცვლადისთვის მნიშვნელობის მინიჭება
+word1 = "welcome"
+word2 = "world"
+# რეინიციალიზება - ცვალდისთვის მნიშვნელობის შეცვლა
+
+word1 = "hello"
+
+# კონკატენაცია - სტრინგების შეერთება
+print(word1 + " " + word2)
