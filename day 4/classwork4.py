7864f368eb033fa576864e478dc6d77d83ac0a44
narimanidzegoga <narimanidzegoga480@gmail.com>
2026-02-13 19:35:07 +0400

    fifth commit

---
+++ day 4/classwork4.py
@@ -0,0 +1,11 @@
+# სახელი,გვარი,ასაკი,მისამართი,ჰობი
+user_name = "goga"
+user_surname = "narimanidze"
+user_age = 15
+user_address = "dusheti"
+user_hobby = "plating football"
+print (user_name)
+print (user_surname)
+print (user_age)
+print (user_address)
+print (user_hobby)
---
+++ day 4/homework.py
@@ -0,0 +1,29 @@
+#1) input - ეს არის რასაც მომხმარებელი წერს პროგრამაში, ხოლო output - რასაც პროგრამა ეკრანზე აჩვენებს. python-ში ვიყენებთ input-ს მონაცემის მისაღებად
+name = input ("enter your name: ")
+print("Hello " + name)
+address = input("enter your address: ")
+print("you live in " + address )
+age = input("Enter your age: ")
+print("Age is " + age)
+food = input("Enter your favorite food: ")
+print("Favorite food is " + food)
+
+country = input("enter the country where you were born in: ")
+print("you were born in " + country)
+# input- მოწყობილობები არის მაგალითად: კლავიატურა,მიკროფონი,მაუსი და მეტი. 
+# output- მოწყობილობებია მონიტორი,ყურსასმენები,პრინტერი და ასე შემდეგ
+# input- მოწყობილობაზე მაგალითად კლავიატურაზე შეგვყავს რიცხვები,სიმბოლოები და ასოები და ეს ყველაფერი გამოისახება მონიტორზე-ერთ ერთ output მოწყობილობაზე
+
+name = input("enter your name: ")
+surname =input("enter your surname: ")
+print("welcome to my website " + name + " " + surname)
+address = input("where do you live?: ")
+print(name + " " + surname + " lives in " + address)
+hobby = input("what is your hobby?: ")
+fav_movie = input("what is your favorite movie?: ")
+print (name + " " + surname + " lives in " + address + ", likes " + hobby + " and loves the movie " + fav_movie )
+
+#case sensitive language- ეს იმას ნიშნავს რომ თუ დიდი ან პატარა ასოები შეცვლილია, კომპიუტერი სრულიად განსხვავებულად აღიქვამს, მაგალითად "A" და "a" სრულიად სხვადასხვაა
+#snake_case- ეს არის კოდის წერის სტილი სადაც სიტყვენი დაყოფილია ქვედა ტირეთი რედგან ადვილად გასაგები იყოს, მაგალითად firstname - აქ არ არის snake case გამოყენებული first_name - აქ კი გამოყენებულია.
+
+
